45facd002a1d7381b4627f6fe871bfc05a29846e
Husush <husush21@gmail.com>
2023-01-04 15:18:56 +0300

    js

---
+++ score.js
@@ -0,0 +1,21 @@
+function breakingRecords(scores) {
+  // Write your code here\
+  let best = 0;
+  let worst = 0;
+  let bestScore = scores[0];
+  let worstScore = scores[0];
+  const lengthOfData = scores.length;
+  for (let i = 1; i < scores.length; i++) {
+    if (scores[i] > bestScore) {
+      bestScore = scores[i];
+      best++;
+      continue;
+    }
+    if (scores[i] < worstScore) {
+      worstScore = scores[i];
+      worst++;
+      continue;
+    }
+  }
+  return [best, worst];
+}
